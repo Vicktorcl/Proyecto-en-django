179383a4078bfb7cd33f8256051d58e87a1e9d02
Vicktorcl <victorrigocl@gmail.com>
2024-06-09 22:08:34 -0400

    Tienda_venv + base y models

--- tienda/core/views.py
+++ tienda/core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from .models import Producto
 
 def inicio(request):
-    return render(request, "core/inicio.html")
+    productos = Producto.objects.all().order_by('nombre')
+    data = { 'productos': productos }
+    return render(request, "core/inicio.html", data)
+
+
+def ficha(request, producto_id):
+    producto = Producto.objects.get(id=producto_id)
+    data = { 'producto': producto }
+    return render(request, "core/ficha.html", data)
 
 def nosotros(request):
     return render(request, 'core/nosotros.html')
 
-def ficha(request):
-    return render(request, "core/ficha.html")
 
 def registro(request):
     return render(request, 'core/registro.html')
